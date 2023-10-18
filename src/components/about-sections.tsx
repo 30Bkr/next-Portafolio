2092f5788310b363c4f89ac3a2c4f8328a62388c
The Briant Carrillo <brianttd24.7@gmail.com>
2023-10-17 21:45:01 -0400

    agreamos imagenes livianas

--- src/components/about-sections.tsx
+++ src/components/about-sections.tsx
@@ -84,7 +84,6 @@ export default function AboutSection()  {
   return (
     <section className='text-white h-fit  sm:pt-12 relative ' id='about'>
       <div className='hidden sm:flex sm:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -right-4 transform translate-x-1/2 translate-1/2'></div>
-
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 z-10'>
           <Image alt='Foto replicada' src="/ejemplo.webp" width={200} height={200}/>
           <div className='mt-4 md:mt-0 text-left flex flex-col  w-full h-[642px]'>
