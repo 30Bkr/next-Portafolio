1bb5d97fa8d3cd5508117081b88c01c76eac9640
The Briant Carrillo <brianttd24.7@gmail.com>
2023-11-13 18:29:33 -0400

    agregamos cambios

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <section className='h-screen pt-24 z-10' id='contact'>
         <div className='flex justify-center items-center mt-20'>
-            <form className='z-20 flex flex-col bg-white h-[550px] w-[400px] rounded-xl px-8 py-8' action="">
-                <h2 className='w-full flex justify-center items-center'>Contactame</h2>
+            <form className='z-20 flex flex-col bg-gray-800 h-[550px] w-[400px] rounded-xl px-8 py-8' action="">
+                <h2 className='w-full flex justify-center items-center text-white'>Contactame</h2>
                 <label className='my-4 w-full' htmlFor="nombre">
                     <input className='w-full bg-blue-300 p-4 rounded-3xl caret-pink-500 placeholder:text-slate-600 border border-stale-400  focus:border-sky-500' type="text" name="nombre" id="nombre" placeholder='Nombre' />
                 </label>
